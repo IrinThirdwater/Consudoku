4eb5295576ad4e0ffe022b612ada5e5103cd4e7c
JirapatTreyanurak <j.treyanurak@gmail.com>
2018-04-30 16:01:32 +0100

    switched over to fxml for layout specification

--- src/JavaFXPrototype.java
+++ src/JavaFXPrototype.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -19,50 +21,18 @@ import javafx.stage.Stage;
  */
 public class JavaFXPrototype extends Application {
 
+    private static final String FXMLSource = "FXMLDocument.fxml";
     //private static final int WIDTH  = 600;
     //private static final int HEIGHT = 500;
 
     @Override
-    public void start (Stage primaryStage) {
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-        //grid.setGridLinesVisible(true);
+    public void start (Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource(FXMLSource));
+        Scene scene = new Scene(root);//, WIDTH, HEIGHT);
 
-        Text sceneTitle = new Text("Welcome!");
-        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        grid.add(sceneTitle, 0, 0, 2, 1);
-
-        Label usernameLabel = new Label("Username:");
-        TextField usernameField = new TextField();
-        grid.add(usernameLabel, 0, 1);
-        grid.add(usernameField, 1, 1);
-
-        Label passwordLabel = new Label("Password:");
-        PasswordField passwordField = new PasswordField();
-        grid.add(passwordLabel, 0, 2);
-        grid.add(passwordField, 1, 2);
-
-        Button btn = new Button("Sign in");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-        hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 1, 4);
-
-        final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 6);
-        btn.setOnAction(e -> {
-            actiontarget.setFill(Color.FIREBRICK);
-            actiontarget.setText("Signed in!");
-        });
-
-        Scene scene = new Scene(grid);//, WIDTH, HEIGHT);
-
-        primaryStage.setTitle("Consudoku");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setTitle("FXML Test");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main (String[] args) {
